7725999d7a0d2109ebeed49a567a2fa6910a9b49
Niilo Keinänen <niilo@ARCTION.local>
2019-09-25 16:32:58 +0300

    Update webpack title

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'LCJS Trading Chart'
+            title: 'LCJS rendering speed showcase'
         })
     ]
 }
